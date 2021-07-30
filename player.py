af4d7b0df7347035d0bdb67b62623ce406a74b60
Milind Sharma <milindsharma8@gmail.com>
2021-07-31 01:50:32 +0800

    Make Player class
    Added all basic methods and attributes.

---
+++ player.py
@@ -0,0 +1,24 @@
+"""Script for Player class."""
+
+from typing import Tuple
+
+
+class Player:
+    def __init__(self) -> None:
+        print('player created!')
+
+    def set_name(self, name: str = ''):
+        self.name = name
+
+    def set_num(self, num: int):
+        self.num = num
+
+    def set_token(self, token: str):
+        self.token = token
+
+    def set_move(self) -> str:
+        move = str(input('Grid space: '))
+        return move
+
+    def tell_grid(self, move: int) -> Tuple[int, str]:
+        return tuple(move, self.token)
