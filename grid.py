35cbb6b38cfacf3c5233c260e68f94d47e6300de
Milind Sharma <milindsharma8@gmail.com>
2021-07-31 17:48:51 +0800

    Create Grid class and basic methods/attributes

---
+++ grid.py
@@ -0,0 +1,18 @@
+"""Script for grid class."""
+
+class Grid:
+    def __init__(self) -> None:
+        self._internal_grid = list(range(9))
+        print('grid created!')
+    
+    def check_valid(self) -> bool:
+        pass
+
+    def set_int_grid(self):
+        pass
+
+    def check_win(self) -> bool:
+        pass
+
+    def check_tie(self) -> bool:
+        pass
