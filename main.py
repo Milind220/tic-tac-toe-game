a4659f0ec44c535ded553a50a67d1504b9a2b524
Milind Sharma <milindsharma8@gmail.com>
2021-08-02 21:21:06 +0800

    Create explicit main script

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Main script for the tic tac toe game.
+
+Contains a function named main, which runs the game when called.
+This script imports classes from the supporting scripts.
+"""
+
+def main() ->  None:
+    pass
+
+if __name__ == '__main__':
+    main()
