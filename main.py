3ace918c06cba593eb56ea1bf695d5fcb52b9cb9
Milind Sharma <milindsharma8@gmail.com>
2021-08-31 21:06:39 +0800

    Add import statements

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ Contains a function named main, which runs the game when called.
 This script imports classes from the supporting scripts.
 """
 
+from display import Display
+from player import Player
+from grid import Grid
+# TODO: look at where to put .imports and where not to put .imports
+
+
 def main() ->  None:
     pass
+
 
 if __name__ == '__main__':
     main()
