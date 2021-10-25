58b269a723149a1bdb5c204365278d0af873bf77
Milind Sharma <milindsharma8@gmail.com>
2021-10-25 20:47:46 +0800

    Create functions.py and import it in main.py

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ This script imports classes from the supporting scripts.
 from display import Display
 from player import Player
 from grid import Grid
-# TODO: look at where to put .imports and where not to put .imports
+import functions
 
 
 def main() ->  None:
     pass
-
 
 if __name__ == '__main__':
     main()
